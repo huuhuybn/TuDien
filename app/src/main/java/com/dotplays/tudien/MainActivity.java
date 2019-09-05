f47550ac44f8e1b471bdfe9c928c243ea576e972
HuyNguyen <huuhuybn@gmail.com>
2019-09-05 11:38:52 +0700

    final code

--- app/src/main/java/com/dotplays/tudien/MainActivity.java
+++ app/src/main/java/com/dotplays/tudien/MainActivity.java
@@ -1,14 +1,23 @@
 package com.dotplays.tudien;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,7 +26,15 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView lvList;
 
 
-    private SqliteCreater sqliteCreater;
+    private DataBaseHelper dataBaseHelper;
+
+    private WordAdapter wordAdapter;
+
+    private LinearLayoutManager linearLayoutManager;
+
+
+    private List<Word> wordList = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,34 +43,46 @@ public class MainActivity extends AppCompatActivity {
         edtText = findViewById(R.id.edtText);
         lvList = findViewById(R.id.lvList);
 
-        sqliteCreater = new SqliteCreater(this);
+        dataBaseHelper = new DataBaseHelper(this);
+        dataBaseHelper.createDataBase();
 
-        // Đừng quên xin quyền ghi bộ nhớ trong file manifest em nhé
 
-        sqliteCreater.copyDatabaseFromAssets();
+        wordAdapter = new WordAdapter(wordList, this);
+        linearLayoutManager = new LinearLayoutManager(this);
 
+        lvList.setAdapter(wordAdapter);
+
+        lvList.setLayoutManager(linearLayoutManager);
+
+
+        edtText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                search(charSequence.toString());
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
 
     }
 
-    public void search(View view) {
+    public void search(String word) {
 
-        String word = edtText.getText().toString().trim();
+        List<Word> wordList = dataBaseHelper.searchWord(word);
 
-        // kiểm tra nếu người dùng chưa nhập gì thì dừng lại và thông báo lỗi
-        if (word.isEmpty()) {
-            edtText.setError("Vui lòng nhập dữ liệu !!!");
-            return;
-            // nếu chữ ko empty thì tiếp tục tìm kiếm và hiển thị danh sách kết quả lên list nếu có
-        } else {
-
-
-            List<Word> results = sqliteCreater.search(
-                    word);
-
-            Log.e("SIZE", results.size() + "");
-
-        }
-
-
+        this.wordList.addAll(wordList);
+        wordAdapter.notifyDataSetChanged();
+        
     }
+
 }
--- app/src/main/java/com/dotplays/tudien/WordAdapter.java
+++ app/src/main/java/com/dotplays/tudien/WordAdapter.java
@@ -1,27 +1,44 @@
 package com.dotplays.tudien;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 public class WordAdapter extends RecyclerView.Adapter<WordHolder> {
 
+    private List<Word> wordList;
 
+    private Context context;
+
+    public WordAdapter(List<Word> wordList, Context context) {
+        this.wordList = wordList;
+        this.context = context;
+    }
 
     @NonNull
     @Override
     public WordHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+
+        View view = LayoutInflater.from(context).inflate(R.layout.word, parent, false);
+
+        return new WordHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull WordHolder holder, int position) {
 
+        holder.tvWord.setText(wordList.get(position).word);
+
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return wordList.size();
     }
 }
